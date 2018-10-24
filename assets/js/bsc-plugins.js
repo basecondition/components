defbd7c68f481887a54c7671b82b5f0d87a8ea05
Joachim Dörr <mail@joachim-doerr.com>
2018-10-24 03:24:32 +0200

    assets updated

--- assets/js/bsc-plugins.js
+++ assets/js/bsc-plugins.js
@@ -7,34 +7,87 @@
  * file that was distributed with this source code.
  */
 
-$(function () {
-    tools_init();
-    $(document).on('pjax:end', function() {
-        tools_init();
-    });
+let bsc_checkbox = '.basecondition-toggle',
+    bsc_multi_select = '.basecondition-multi-select',
+    bsc_multi_open_select = '.basecondition-multi-select-open',
+    bsc_dropdown = '.base_mblock .dropdown',
+    form_element = '.base-form';
+
+$(document).on('ready', function () {
+    if (typeof mblock_module === 'object') {
+        mblock_module.registerCallback('reindex_end', function () {
+            if (mblock_module.lastAction === 'add_item') {
+                tools_destroy(mblock_module.affectedItem);
+                tools_init(mblock_module.affectedItem);
+            }
+        });
+    }
 });
 
-function tools_init() {
-    var multi_select = $('.basecondition-multi-select'),
-        multi_open_select = $('.basecondition-multi-select-open');
-    if (multi_select.length) {
-        multi_select.each(function () {
+$(document).on('ready pjax:success', function () {
+    tools_init($(form_element));
+});
+
+function tools_init($element) {
+    mblock_dropdown_init($element);
+    checkboxpicker_init($element);
+    multiselects_init($element);
+}
+
+function tools_destroy($element) {
+    checkboxpicker_destroy($element);
+    multiselects_destroy($element);
+}
+
+function multiselects_init($element) {
+    if ($element.find(bsc_multi_select).length) {
+        $element.find(bsc_multi_select).each(function () {
+            var maxheight = 350,
+                numberdisplay = 3,
+                search = 1,
+                selectall = 1;
+            if (typeof $(this).data('max-height') !== 'undefined') {
+                maxheight = $(this).data('max-height');
+            }
+            if (typeof $(this).data('number-display') !== 'undefined') {
+                numberdisplay = $(this).data('number-display');
+            }
+            if (typeof $(this).data('search') !== 'undefined') {
+                search = $(this).data('search');
+            }
+            if (typeof $(this).data('select-all') !== 'undefined') {
+                selectall = $(this).data('select-all');
+            }
             $(this).baseconditionMultiselect({
-                includeSelectAllOption: true,
+                includeSelectAllOption: selectall,
                 selectAllText: 'Select all',
                 filterBehavior: 'both',
-                enableFiltering: true,
-                maxHeight: 350
+                enableFiltering: search,
+                maxHeight: maxheight,
+                numberDisplayed: numberdisplay,
             });
         });
     }
-    if (multi_open_select.length) {
-        multi_open_select.each(function () {
+    if ($element.find(bsc_multi_open_select).length) {
+        $element.find(bsc_multi_open_select).each(function () {
+            var maxheight = false,
+                search = 1,
+                selectall = 1;
+            if (typeof $(this).data('max-height') !== 'undefined') {
+                maxheight = $(this).data('max-height');
+            }
+            if (typeof $(this).data('search') !== 'undefined') {
+                search = $(this).data('search');
+            }
+            if (typeof $(this).data('select-all') !== 'undefined') {
+                selectall = $(this).data('select-all');
+            }
             $(this).baseconditionMultiselect({
-                includeSelectAllOption: true,
+                includeSelectAllOption: selectall,
                 selectAllText: 'Select all',
                 filterBehavior: 'both',
-                enableFiltering: true,
+                enableFiltering: search,
+                maxHeight: maxheight,
                 templates: {
                     button: '',
                     ul: '<ul class="bsc-multiselect-container checkbox-list"></ul>',
@@ -47,14 +100,26 @@ function tools_init() {
             });
         });
     }
-
-    store_mblock_init();
 }
 
-function checkboxpicker_init() {
-    var checkbox = $('.basecondition-toggle');
-    if (checkbox.length) {
-        checkbox.each(function () {
+function multiselects_destroy($element) {
+    multiselect_destroy($element, bsc_multi_select);
+    multiselect_destroy($element, bsc_multi_open_select);
+}
+
+function multiselect_destroy($element, $select) {
+    if ($element.find($select).length) {
+        $element.find($select).each(function () {
+            var parent_element = $(this).parent();
+            $(this).appendTo($(this).parents('dd'));
+            parent_element.remove();
+        });
+    }
+}
+
+function checkboxpicker_init($element) {
+    if ($element.find(bsc_checkbox).length) {
+        $element.find(bsc_checkbox).each(function () {
             $(this).baseconditionToggle();
             var val = 0;
             if ($(this).prop('checked')) {
@@ -64,7 +129,7 @@ function checkboxpicker_init() {
             $(this).parent().parent().append('<input type="hidden" name="' + $(this).attr('name').replace('[1]', '') + '" value="' + val + '"/>');
             $(this).attr('name', '');
 
-            $(this).change(function() {
+            $(this).change(function () {
                 var val = 0;
                 if ($(this).prop('checked')) {
                     val = 1;
@@ -75,14 +140,21 @@ function checkboxpicker_init() {
     }
 }
 
-function store_mblock_init() {
-    var af_dropdown = $('.base_mblock .dropdown');
-    if (af_dropdown.length) {
-        af_dropdown.each(function() {
+function checkboxpicker_destroy($element) {
+    if ($element.find(bsc_dropdown).length) {
+        $element.find(bsc_dropdown).each(function () {
+
+        });
+    }
+}
+
+function mblock_dropdown_init($element) {
+    if ($element.find(bsc_dropdown).length) {
+        $element.find(bsc_dropdown).each(function () {
             var target = $(this).parent().next();
-            $(this).find('a').each(function(){
+            $(this).find('a').each(function () {
                 // ajax
-                $(this).unbind().bind('click', function(event){
+                $(this).unbind().bind('click', function (event) {
                     var _li = $(this).parent(),
                         _a = $(this);
 
@@ -93,6 +165,7 @@ function store_mblock_init() {
                             success: function (result) {
                                 target.append(result);
                                 mblock_init();
+                                tools_init(target.find('.mblock_wrapper'));
 
                                 _li.addClass('disabled');
                                 _a.find('span').remove();
@@ -106,13 +179,13 @@ function store_mblock_init() {
                 })
             });
         });
-        store_mblock_callback_run(af_dropdown);
+        mblock_dropdown_callback_run($element.find(bsc_dropdown));
     }
 }
 
-function store_mblock_callback_run(af_dropdown) {
+function mblock_dropdown_callback_run($element) {
     mblock_module.registerCallback('reindex_end', function (item) {
-        af_dropdown.each(function () {
+        $element.each(function () {
             var _drop = $(this),
                 target = $(this).parent().next();
             target.find('.mblock_wrapper').each(function () {
